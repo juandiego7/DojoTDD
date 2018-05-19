d8929443e0608b759aca2f7a2ca2c9f6c4a65947
juandiego7 <jdiego7118@gmail.com>
2018-05-19 11:33:39 -0500

    Test services

--- app/controllers/Codebreaker.scala
+++ app/controllers/Codebreaker.scala
@@ -3,21 +3,39 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class Codebreaker {
+class Codebreaker @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+    def setSecret() = Action {  implicit request =>
+        val bodyAsJson = request.body.asJson.get
+        secret = (bodyAsJson \ "secret").asOpt[String].get
+        Ok("success")
+    }
+        
+    def trySecretService(num: String) = Action { 
+        val res = trySecret(num)
+        val json = Json.toJson(res)
+        result = ""
+        xs = ""
+        underScores = ""
+        Ok(json)
+    }
+   
+    
     var secret = "1234"
     var xs = ""
     var underScores = ""
+    var result:String = ""
 
   def trySecret(num: String): String = {
 
-    var result:String = ""
+    
 
     var i = 0
     var j = 0
--- test/controllers/CodebreakerTest.scala
+++ test/controllers/CodebreakerTest.scala
@@ -6,6 +6,7 @@ import play.api.test._
 import play.api.test.Helpers._
 import scala.concurrent.Future
 import play.api.mvc._
+import play.api.libs.json._
 
 /**
  * Add your spec here.
@@ -19,7 +20,7 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando 1234" should {
     "should be exist" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("1234")
         result mustBe "xxxx"
     }
@@ -28,7 +29,7 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando 1235" should {
     "should be valid" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("1235")
         result mustBe "xxx"
     }
@@ -37,7 +38,7 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando 1246" should {
     "should be valid" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("1246")
         result mustBe "xx_"
     }
@@ -46,7 +47,7 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando 5678" should {
     "should be valid" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("5678")
         result mustBe ""
     }
@@ -55,7 +56,7 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando 1243" should {
     "should be valid" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("1243")
         result mustBe "xx__"
     }
@@ -64,10 +65,29 @@ class CodebreakerTest extends PlaySpec with Results {
   "Probando mas de cuatro numeros" should {
     "should be valid" in {
 
-        val c = new Codebreaker()
+        val c = new Codebreaker(stubControllerComponents())
         val result = c.trySecret("12435")
         result mustBe "No ingrese mas de 4 digitos"
     }
+  }
+
+  "Test service /answer/1234" in {
+    val controller = new Codebreaker(stubControllerComponents())
+    val home = controller.trySecretService("1234").apply(FakeRequest(GET, "/answer/1234"))
+
+    status(home) mustBe OK
+    contentType(home) mustBe Some("application/json")
+    contentAsString(home) must include ("xxxx")
+  }
+
+  "Test service /setsecret 1235" in {
+    val controller = new Codebreaker(stubControllerComponents())
+    val home = controller.setSecret().apply(FakeRequest(POST, "/setsecret")
+                    .withJsonBody(Json.parse("""{ "secret": "1235" }""")))
+
+    status(home) mustBe OK
+    contentType(home) mustBe Some("text/plain")
+    contentAsString(home) must include ("success")
   }
   
 }
